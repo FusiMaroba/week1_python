59452ad612e20124ba70ed425de9843d3b695531
KelvinMaroba <kelvinmarobabusiness@gmail.com>
2024-08-28 02:48:10 +0200

    First commit

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Please provide your name: ")
+age = input("Please provide your age: ")
+location = input("Where do you live: ")
+
+print(f"Good day {name}, you are {age} years old. You live in {location}")
